def4ebc0040698bfc474336643efdb7aed34af99
胡国亮 <g17254172@gmail.com>
2023-07-13 16:47:16 +0800

    回收时重置parent

--- Scripts/Runtime/Entity/EntityLogic.cs
+++ Scripts/Runtime/Entity/EntityLogic.cs
@@ -117,6 +117,7 @@ namespace UnityGameFramework.Runtime
         /// </summary>
         protected internal virtual void OnRecycle()
         {
+            CachedTransform.SetParent(m_OriginalTransform);
         }
 
         /// <summary>
